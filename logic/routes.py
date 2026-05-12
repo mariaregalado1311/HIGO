c07be8c1b0ecdc2542520ea8476ba78dfaa3cd45
mariaregalado1311 <mariaregalado1311@hotmail.com>
2026-05-12 20:03:29 +0200

    Update routes.py

--- logic/routes.py
+++ logic/routes.py
@@ -2,11 +2,5 @@ from config import ROUTE_STEPS
 
 
 def get_low_stress_route() -> list[str]:
-    """
-    Retorna pasos de ruta tolerable.
-
-    Outputs:
-        list[str]
-    """
 
     return ROUTE_STEPS
