c0253dfd765e9ef96faf8ea19a40f2598a8fdeaf
mariaregalado1311 <mariaregalado1311@hotmail.com>
2026-05-12 20:03:56 +0200

    Update sensory.py

--- logic/sensory.py
+++ logic/sensory.py
@@ -5,24 +5,10 @@ def calculate_sensory_score(
     noise_level: int,
     crowd_level: int
 ) -> int:
-    """
-    Calcula score sensorial.
 
-    Inputs:
-        noise_level: sensibilidad ruido
-        crowd_level: sensibilidad crowd
-
-    Outputs:
-        int score
-
-    Raises:
-        ValueError si inputs inválidos
-    """
-
-    if noise_level < 1 or crowd_level < 1:
-        raise ValueError("Valores inválidos.")
-
-    reduction = (noise_level + crowd_level) // 2
+    reduction = (
+        noise_level + crowd_level
+    ) // 2
 
     score = DEFAULT_SENSORY_SCORE - reduction
 
