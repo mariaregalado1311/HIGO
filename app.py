8c260cdb0ea9c20a83dbdc608318a7264516910a
mariaregalado1311 <mariaregalado1311@hotmail.com>
2026-05-12 20:01:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import streamlit as st
+
 from data.database import initialize_database
 from ui._brand import inject_brand_styles
 from ui.home import render_home_page
 
 
 def main() -> None:
-    """
-    Entry point aplicación.
-    """
+
+    st.set_page_config(
+        page_title="NeuroRoute",
+        page_icon="🧠",
+        layout="wide"
+    )
 
     initialize_database()
 
